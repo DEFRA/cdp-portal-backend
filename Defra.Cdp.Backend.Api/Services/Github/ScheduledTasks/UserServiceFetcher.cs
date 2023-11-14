5b21d24a0df4d9e7cc01617285f20a8101750e79
Mohammed Abdulrazeg <github@mzawi.me>
2023-11-14 10:18:02 +0000

    Remove weak ssl check when calling user service backend

--- Defra.Cdp.Backend.Api/Services/Github/ScheduledTasks/UserServiceFetcher.cs
+++ Defra.Cdp.Backend.Api/Services/Github/ScheduledTasks/UserServiceFetcher.cs
@@ -6,20 +6,14 @@ namespace Defra.Cdp.Backend.Api.Services.Github.ScheduledTasks;
 public class UserServiceFetcher
 {
     private readonly HttpClient _client;
-    private readonly string _userServiceBackendUrl;
 
     public UserServiceFetcher(IConfiguration configuration)
     {
-        _userServiceBackendUrl = configuration.GetValue<string>("UserServiceBackendUrl")!;
-        if (_userServiceBackendUrl == null)
+        var userServiceBackendUrl = configuration.GetValue<string>("UserServiceBackendUrl")!;
+        if (userServiceBackendUrl == null)
             throw new ArgumentNullException("_userServiceBackendUrl", "User service backend url cannot be null");
-        // TODO REMOVE ME BEFORE MERGING IN
-        var handler = new HttpClientHandler();
-        handler.ClientCertificateOptions = ClientCertificateOption.Manual;
-        handler.ServerCertificateCustomValidationCallback =
-            (httpRequestMessage, cert, cetChain, policyErrors) => { return true; };
-        _client = new HttpClient(handler);
-        _client.BaseAddress = new Uri(_userServiceBackendUrl);
+        _client = new HttpClient();
+        _client.BaseAddress = new Uri(userServiceBackendUrl);
         _client.DefaultRequestHeaders.Accept.Clear();
         _client.DefaultRequestHeaders.Add(
             "Accept", "application/json");
