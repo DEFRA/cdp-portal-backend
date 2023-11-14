4dcfdcf4866c07804bfaa862c8124e6f93efd213
Mohammed Abdulrazeg <github@mzawi.me>
2023-11-14 08:56:05 +0000

    Using `name` instead of `TeamName`

--- Defra.Cdp.Backend.Api.Tests/Endpoints/HelpersTests.cs
+++ Defra.Cdp.Backend.Api.Tests/Endpoints/HelpersTests.cs
@@ -1,17 +1,21 @@
 using System.Security.Claims;
 using Defra.Cdp.Backend.Api.Endpoints;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using NSubstitute;
 
 namespace Defra.Cdp.Backend.Api.Tests.Endpoints;
 
 public class HelpersTests
 {
+    private readonly ILoggerFactory _loggerFactory = new LoggerFactory();
+
+
     [Fact]
     public void GetGroups_ReturnsGroups_WhenClaimExists()
     {
         // Arrange
-        var claims = new List<Claim> { new("groups", "[\"Group1\", \"Group2\"]") };
+        var claims = new List<Claim> { new("groups", "Group1"), new("groups", "Group2") };
         var identity = new ClaimsIdentity(claims, "TestAuthType");
         var user = new ClaimsPrincipal(identity);
 
@@ -19,7 +23,7 @@ public class HelpersTests
         httpContext.User.Returns(user);
 
         // Act
-        var groups = Helpers.ExtractGroups(httpContext);
+        var groups = Helpers.ExtractGroups(httpContext, _loggerFactory)!;
 
         // Assert
         Assert.Equal(2, groups.Count);
@@ -35,7 +39,7 @@ public class HelpersTests
         httpContext.User.Returns(new ClaimsPrincipal());
 
         // Act
-        var groups = Helpers.ExtractGroups(httpContext);
+        var groups = Helpers.ExtractGroups(httpContext, _loggerFactory);
 
         // Assert
         Assert.Null(groups);
