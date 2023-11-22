fb8f1b620996c2e2c0040e8ada75c64eacf4da0c
Mohammed Abdulrazeg <github@mzawi.me>
2023-11-22 17:29:11 +0000

    Recreate reader on demand

--- Defra.Cdp.Backend.Api/Services/Github/ScheduledTasks/Base64StringPrivateKeySource.cs
+++ Defra.Cdp.Backend.Api/Services/Github/ScheduledTasks/Base64StringPrivateKeySource.cs
@@ -5,16 +5,18 @@ namespace Defra.Cdp.Backend.Api.Services.Github.ScheduledTasks;
 
 public class Base64StringPrivateKeySource : IPrivateKeySource
 {
-    private readonly TextReader _keyReader;
+    private TextReader _keyReader;
+    private readonly string _key;
 
     public Base64StringPrivateKeySource(string encodedPem)
     {
-        var key = Encoding.UTF8.GetString(Convert.FromBase64String(encodedPem));
-        _keyReader = new StringReader(key);
+        _key = Encoding.UTF8.GetString(Convert.FromBase64String(encodedPem));
+        _keyReader = new StringReader(_key);
     }
 
     public TextReader GetPrivateKeyReader()
     {
+        _keyReader = new StringReader(_key);
         return _keyReader;
     }
 }
