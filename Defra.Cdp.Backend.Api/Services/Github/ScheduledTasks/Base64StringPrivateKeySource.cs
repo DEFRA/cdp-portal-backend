82084793877d528b0efa80c08c179d9e5dcb7a4f
Mohammed Abdulrazeg <github@mzawi.me>
2023-11-22 17:32:17 +0000

    better code styling

--- Defra.Cdp.Backend.Api/Services/Github/ScheduledTasks/Base64StringPrivateKeySource.cs
+++ Defra.Cdp.Backend.Api/Services/Github/ScheduledTasks/Base64StringPrivateKeySource.cs
@@ -5,18 +5,15 @@ namespace Defra.Cdp.Backend.Api.Services.Github.ScheduledTasks;
 
 public class Base64StringPrivateKeySource : IPrivateKeySource
 {
-    private TextReader _keyReader;
     private readonly string _key;
 
     public Base64StringPrivateKeySource(string encodedPem)
     {
         _key = Encoding.UTF8.GetString(Convert.FromBase64String(encodedPem));
-        _keyReader = new StringReader(_key);
     }
 
     public TextReader GetPrivateKeyReader()
     {
-        _keyReader = new StringReader(_key);
-        return _keyReader;
+        return new StringReader(_key);
     }
 }
