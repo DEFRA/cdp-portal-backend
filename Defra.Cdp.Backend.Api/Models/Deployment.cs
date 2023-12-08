d431012a65ebea65771324f399de2e17a61f43a9
Mohammed Abdulrazeg <github@mzawi.me>
2023-12-08 08:47:55 +0000

    squashed-deployment pagination (#49)

--- Defra.Cdp.Backend.Api/Models/Deployment.cs
+++ Defra.Cdp.Backend.Api/Models/Deployment.cs
@@ -27,7 +27,7 @@ public sealed class Deployment
 
     public string? InstanceTaskId { get; init; }
 
-    public int? InstanceCount { get; init; } = 1; // default value is 1 if we don't get it from legacy calls
+    public int? InstanceCount { get; init; } = -1; // default value is -1 if we don't get it from legacy calls
 }
 
 public sealed class DeploymentsPage
@@ -46,6 +46,50 @@ public sealed class DeploymentsPage
     public int TotalPages { get; init; }
 
     public void Deconstruct(out List<Deployment> deployments, out int page, out int pageSize, out int totalPages)
+    {
+        deployments = Deployments;
+        page = Page;
+        pageSize = PageSize;
+        totalPages = TotalPages;
+    }
+}
+
+public sealed class SquashedDeployment
+{
+    public string DeploymentId { get; init; } = default!;
+    public string Environment { get; init; } = default!;
+    public string Service { get; init; } = default!;
+    public string Version { get; init; } = default!;
+
+    public string? User { get; init; }
+    public string? UserId { get; init; }
+
+    public DateTime DeployedAt { get; init; } = default!;
+    public string Status { get; init; } = default!;
+    public string DockerImage { get; init; } = default!;
+    public string? TaskId { get; init; }
+
+    public int Count { get; init; }
+    public int? RequestedCount { get; init; } = -1; // default value is -1 if we don't get it from legacy calls
+}
+
+public sealed class SquashedDeploymentsPage
+{
+    public SquashedDeploymentsPage(List<SquashedDeployment> deployments, int page, int pageSize, int totalPages)
+    {
+        Deployments = deployments;
+        Page = page;
+        PageSize = pageSize;
+        TotalPages = totalPages;
+    }
+
+    public List<SquashedDeployment> Deployments { get; init; }
+    public int Page { get; init; }
+    public int PageSize { get; init; }
+    public int TotalPages { get; init; }
+
+    public void Deconstruct(out List<SquashedDeployment> deployments, out int page, out int pageSize,
+        out int totalPages)
     {
         deployments = Deployments;
         page = Page;
--- Defra.Cdp.Backend.Api/Services/Tenants/DeploymentsService.cs
+++ Defra.Cdp.Backend.Api/Services/Tenants/DeploymentsService.cs
@@ -12,12 +12,16 @@ public interface IDeploymentsService
     public Task<DeploymentsPage> FindLatest(string? environment, int offset = 0, int page = 0, int size = 0,
         CancellationToken cancellationToken = new());
 
+    public Task<SquashedDeploymentsPage> FindLatestSquashed(string? environment, int offset = 0, int page = 0,
+        int size = 0,
+        CancellationToken cancellationToken = new());
+
     public Task<List<Deployment>> FindWhatsRunningWhere(CancellationToken cancellationToken);
     public Task<List<Deployment>> FindWhatsRunningWhere(string serviceName, CancellationToken cancellationToken);
     public Task<Deployment?> FindDeployment(string deploymentId, CancellationToken cancellationToken);
 
     public Task<Deployment?> FindRequestedDeployment(string service, string version, string environment,
-        DateTime deployedAt, string taskId, CancellationToken cancellationToken);
+        DateTime deployedAt, string deploymentId, CancellationToken cancellationToken);
 
     public Task<UpdateResult> LinkRequestedDeployment(ObjectId? id, Deployment deployment,
         CancellationToken cancellationToken);
@@ -62,6 +66,44 @@ public class DeploymentsService : MongoService<Deployment>, IDeploymentsService
         return new DeploymentsPage(deployments, page, size, totalPages);
     }
 
+    public async Task<SquashedDeploymentsPage> FindLatestSquashed(string? environment, int offset = 0, int page = 0,
+        int size = 0,
+        CancellationToken cancellationToken = new())
+    {
+        page = page == 0 ? DefaultPage : page;
+        size = size == 0 ? DefaultPageSize : size;
+        if (size <= 0) throw new ArgumentException("page size cannot be less than zero");
+        var pipeline = new EmptyPipelineDefinition<Deployment>()
+            .Sort(new SortDefinitionBuilder<Deployment>().Descending(d => d.DeployedAt))
+            .Group(
+                d => new { d.Status, d.TaskId, d.InstanceCount },
+                grp =>
+                    new
+                    {
+                        Root = new SquashedDeployment
+                        {
+                            Count = grp.Count(),
+                            DeployedAt = grp.Max(d => d.DeployedAt),
+                            DeploymentId = grp.First().DeploymentId,
+                            DockerImage = grp.First().DockerImage,
+                            Environment = grp.First().Environment,
+                            RequestedCount = grp.First().InstanceCount,
+                            Service = grp.First().Service,
+                            Version = grp.First().Version,
+                            Status = grp.First().Status,
+                            TaskId = grp.First().TaskId,
+                            User = grp.First().User,
+                            UserId = grp.First().UserId
+                        }
+                    })
+            .Project(p => p.Root);
+
+        var result = await Collection.Aggregate(pipeline, cancellationToken: cancellationToken)
+            .ToListAsync(cancellationToken);
+
+        return new SquashedDeploymentsPage(result!, page, size, 100);
+    }
+
     public Task<Deployment?> FindDeployment(string deploymentId, CancellationToken cancellationToken)
     {
         return Collection.Find(d => d.DeploymentId == deploymentId).SortBy(d => d.DeployedAt)
@@ -72,10 +114,10 @@ public class DeploymentsService : MongoService<Deployment>, IDeploymentsService
     // We look for any task that matches the name/version/env and happened up to 30 minutes before the first deployment 
     // event. 
     public async Task<Deployment?> FindRequestedDeployment(string service, string version, string environment,
-        DateTime timestamp, string taskId, CancellationToken cancellationToken)
+        DateTime timestamp, string deploymentId, CancellationToken cancellationToken)
     {
         // See if we've already matched the requested deployment via deployment id
-        var requested = await Collection.Find(d => d.TaskId == taskId && d.Status == "REQUESTED")
+        var requested = await Collection.Find(d => d.DeploymentId == deploymentId && d.Status == "REQUESTED")
             .FirstOrDefaultAsync(cancellationToken);
 
         if (requested != null) return requested;
@@ -107,7 +149,8 @@ public class DeploymentsService : MongoService<Deployment>, IDeploymentsService
     {
         var pipeline = new EmptyPipelineDefinition<Deployment>().Match(d => d.Service == serviceName)
             .Sort(new SortDefinitionBuilder<Deployment>().Descending(d => d.DeployedAt))
-            .Group(d => new { d.Environment }, grp => new { Root = grp.First() }).Project(grp => grp.Root);
+            .Group(d => new { d.Environment }, grp => new { Root = grp.First() })
+            .Project(grp => grp.Root);
 
         return await Collection.Aggregate(pipeline, cancellationToken: cancellationToken)
             .ToListAsync(cancellationToken);
@@ -145,9 +188,14 @@ public class DeploymentsService : MongoService<Deployment>, IDeploymentsService
         IndexKeysDefinitionBuilder<Deployment> builder)
     {
         var indexModel = new CreateIndexModel<Deployment>(builder.Combine(
-            builder.Ascending(r => r.Environment),
-            builder.Ascending(r => r.Service)));
-        var titleModel = new CreateIndexModel<Deployment>(builder.Descending(r => r.DeployedAt));
-        return new List<CreateIndexModel<Deployment>> { indexModel, titleModel };
+            builder.Ascending(d => d.Environment),
+            builder.Ascending(d => d.Service)));
+        var titleModel = new CreateIndexModel<Deployment>(builder.Descending(d => d.DeployedAt));
+        var instanceCount = new CreateIndexModel<Deployment>(builder.Descending(d => d.InstanceCount),
+            new CreateIndexOptions { Sparse = true });
+        var task = new CreateIndexModel<Deployment>(
+            builder.Combine(builder.Ascending(d => d.TaskId), builder.Ascending(d => d.InstanceTaskId)),
+            new CreateIndexOptions { Sparse = true });
+        return new List<CreateIndexModel<Deployment>> { indexModel, titleModel, instanceCount, task };
     }
 }
