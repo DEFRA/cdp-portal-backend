a110d0cc5dc64733dc91e915884387d623dd7eec
Ben Chidgey <feedmypixel@users.noreply.github.com>
2024-05-17 11:15:58 +0100

    265717 add filtering (#147)
    * Add Search and filters support
    
    - Add search support to the /v2/deployments endpoint
    - Add filters support to the /v2/deployments endpoint
    
    * Add filters endpoint to provide filter information
    
    - Add /v2/deployments/filters/ endpoint to support the UI filters
    
    * Update internal to private
    
    * Filters model

--- Defra.Cdp.Backend.Api/Endpoints/DeploymentsEndpointV2.cs
+++ Defra.Cdp.Backend.Api/Endpoints/DeploymentsEndpointV2.cs
@@ -12,19 +12,26 @@ public static class DeploymentsEndpointV2
         app.MapGet("/v2/deployments",
             async (IDeploymentsServiceV2 deploymentsService,
                 CancellationToken cancellationToken,
-                [FromQuery(Name = "offset")] int? offset,
                 [FromQuery(Name = "environment")] string? environment,
+                [FromQuery(Name = "service")] string? service,
+                [FromQuery(Name = "user")] string? user,
+                [FromQuery(Name = "status")] string? status,
+                [FromQuery(Name = "offset")] int? offset,
                 [FromQuery(Name = "page")] int? page,
                 [FromQuery(Name = "size")] int? size
-            ) =>
-            {
-                var o = offset ?? 0;
-                var p = page ?? DeploymentsServiceV2.DefaultPage;
-                var s = size ?? DeploymentsServiceV2.DefaultPageSize;
-                return await FindLatestDeployments(deploymentsService, environment, o, p, s, cancellationToken);
-            });
+            ) => await FindLatestDeployments(deploymentsService,
+                environment,
+                service,
+                user,
+                status,
+                offset ?? 0,
+                page ?? DeploymentsServiceV2.DefaultPage,
+                size ?? DeploymentsServiceV2.DefaultPageSize,
+                cancellationToken
+            ));
 
         app.MapGet("/v2/deployments/{deploymentId}", FindDeployments);
+        app.MapGet("/v2/deployments/filters/", GetFilters);
         app.MapGet("/v2/whats-running-where", WhatsRunningWhere);
         app.MapGet("/v2/whats-running-where/{service}", WhatsRunningWhereForService);
         app.MapPost("/v2/deployments", RegisterDeployment);
@@ -33,20 +40,37 @@ public static class DeploymentsEndpointV2
         return app;
     }
 
-    // GET /deployments or GET /deployments?offet=23
-    internal static async Task<IResult> FindLatestDeployments(IDeploymentsServiceV2 deploymentsService,
+    // GET /deployments or with query params GET /deployments?environment=dev&service=forms-runner&page=1&offset=0&size=50
+    private static async Task<IResult> FindLatestDeployments(IDeploymentsServiceV2 deploymentsService,
         string? environment,
+        string? service,
+        string? user,
+        string? status,
         int offset,
         int page,
         int size,
         CancellationToken cancellationToken)
     {
-        var deploymentsPage = await deploymentsService.FindLatest(environment, offset, page, size, cancellationToken);
+        var deploymentsPage = await deploymentsService.FindLatest(environment, service, user,
+            status,
+            offset,
+            page,
+            size,
+            cancellationToken
+        );
         return Results.Ok(deploymentsPage);
     }
 
+    // GET /v2/deployments/filters
+    private static async Task<IResult> GetFilters(IDeploymentsServiceV2 deploymentsService,
+        CancellationToken cancellationToken)
+    {
+        var deploymentFilters = await deploymentsService.GetFilters(cancellationToken);
+        return Results.Ok(new { Filters = deploymentFilters });
+    }
+
     // Get /deployments/{deploymentId}
-    internal static async Task<IResult> FindDeployments(IDeploymentsServiceV2 deploymentsService, string deploymentId,
+    private static async Task<IResult> FindDeployments(IDeploymentsServiceV2 deploymentsService, string deploymentId,
         CancellationToken cancellationToken)
     {
         var deployment = await deploymentsService.FindDeployment(deploymentId, cancellationToken);
@@ -56,7 +80,7 @@ public static class DeploymentsEndpointV2
             : Results.Ok(deployment);
     }
 
-    internal static async Task<IResult> WhatsRunningWhere(IDeploymentsServiceV2 deploymentsService,
+    private static async Task<IResult> WhatsRunningWhere(IDeploymentsServiceV2 deploymentsService,
         HttpContext httpContext,
         CancellationToken cancellationToken)
     {
@@ -65,14 +89,14 @@ public static class DeploymentsEndpointV2
         return Results.Ok(deployments);
     }
 
-    internal static async Task<IResult> WhatsRunningWhereForService(IDeploymentsServiceV2 deploymentsService,
+    private static async Task<IResult> WhatsRunningWhereForService(IDeploymentsServiceV2 deploymentsService,
         string service, CancellationToken cancellationToken)
     {
         var deployments = await deploymentsService.FindWhatsRunningWhere(service, cancellationToken);
         return Results.Ok(deployments);
     }
 
-    internal static async Task<IResult> RegisterDeployment(
+    private static async Task<IResult> RegisterDeployment(
         IDeploymentsServiceV2 deploymentsServiceV2,
         IValidator<RequestedDeployment> validator,
         RequestedDeployment rd,
