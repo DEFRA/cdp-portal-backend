b3f328e15fd8083f65660b5144c3340e33a22051
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-07-02 14:24:10 +0100

    removes container lookup

--- Defra.Cdp.Backend.Api/Services/Aws/Deployments/DeploymentEventHandlerV2.cs
+++ Defra.Cdp.Backend.Api/Services/Aws/Deployments/DeploymentEventHandlerV2.cs
@@ -91,10 +91,12 @@ public class DeploymentEventHandlerV2
                 _logger.LogWarning($"Failed to find a matching deployment for {lambdaId}, it may have been triggered by a different instance of portal", lambdaId);
                 return;
             }
-            _logger.LogInformation("Updating deployment {}", JsonSerializer.Serialize(deployment));
 
             // TODO: match on sha256 instead
-            var container = ecsEvent.Detail.Containers.FirstOrDefault(c => c.Image.EndsWith(artifact.Repo + ":" + artifact.Tag));
+            var tagSuffix = artifact.Repo + ":" + artifact.Tag;
+            var shaSuffix = artifact.Repo + "@" + artifact.Sha256.ToLower();
+            
+            var container = ecsEvent.Detail.Containers.FirstOrDefault(c => c.Image.EndsWith(tagSuffix) || c.Image.EndsWith(shaSuffix));
             if (container == null)
             {
                 throw new Exception( $"Failed to find the ECS container entry for {artifact.Repo}:{artifact.Tag}");
