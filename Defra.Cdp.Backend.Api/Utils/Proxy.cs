095107b1e134ff9d00c9b3c71792129f4e89079e
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-07-12 15:22:27 +0100

    gets proxy creds from url

--- Defra.Cdp.Backend.Api/Utils/Proxy.cs
+++ Defra.Cdp.Backend.Api/Utils/Proxy.cs
@@ -13,21 +13,21 @@ public static class Proxy
         {
             // Note: HTTPS proxy isn't support in dotnet until dotnet 8
             var proxyUri = Environment.GetEnvironmentVariable("CDP_HTTP_PROXY");
-            var proxy = new System.Net.WebProxy
+            var proxy = new WebProxy
             {
                 BypassProxyOnLocal = true
             };
             if (proxyUri != null)
             {
-                logger.Information("Creating proxy http client");
-                proxy.Address = new Uri(proxyUri);
-               
-                var proxyUsername = Environment.GetEnvironmentVariable("SQUID_USERNAME");
-                var proxyPassword = Environment.GetEnvironmentVariable("SQUID_PASSWORD");
-                if (proxyUsername != null && proxyPassword != null)
+                var uri = new Uri(proxyUri);
+                logger.Information("Creating proxy http client {uri}", RedactUriCredentials(uri));
+                proxy.Address = uri;
+
+                var credentials = GetCredentialsFromUri(uri) ?? GetCredentialsFromEnv();
+                if (credentials != null)
                 {
                     logger.Information("Setting proxy credentials");
-                    proxy.Credentials = new NetworkCredential(proxyUsername, proxyPassword);
+                    proxy.Credentials = credentials;
                 }
             }
             else
@@ -36,5 +36,33 @@ public static class Proxy
             }
             return new HttpClientHandler { Proxy = proxy, UseProxy = proxyUri != null};
         });
+    }
+    
+    public static NetworkCredential? GetCredentialsFromUri(Uri uri)
+    {
+        var split = uri.UserInfo.Split(':');
+        return split.Length == 2 ? new NetworkCredential(split[0], split[1]) : null;
+    }
+    
+    private static NetworkCredential? GetCredentialsFromEnv()
+    {
+        var proxyUsername = Environment.GetEnvironmentVariable("SQUID_USERNAME");
+        var proxyPassword = Environment.GetEnvironmentVariable("SQUID_PASSWORD");
+        if (proxyUsername != null && proxyPassword != null)
+        {
+            return new NetworkCredential(proxyUsername, proxyPassword);
+        }
+
+        return null;
+    }
+    
+    public static string RedactUriCredentials(Uri uri)
+    {
+        var uriBuilder = new UriBuilder(uri);
+        if (!string.IsNullOrEmpty(uriBuilder.Password))
+        {
+            uriBuilder.Password = "*****";
+        }
+        return uriBuilder.Uri.ToString();
     }
 }
