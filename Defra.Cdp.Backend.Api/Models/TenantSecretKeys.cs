07d37d3d379cd4503159922d03ceafec9fe0b2a8
Ben Chidgey <ben@feedmypixel.com>
2024-08-05 16:36:25 +0100

    Update Name of model to be more generic
    This is now used in multiple palces

--- Defra.Cdp.Backend.Api/Models/TenantSecretKeys.cs
+++ Defra.Cdp.Backend.Api/Models/TenantSecretKeys.cs
@@ -5,7 +5,7 @@ namespace Defra.Cdp.Backend.Api.Models;
 /**
  * A cut down version of TenantSecrets
  */
-public record DeploymentSecrets
+public record TenantSecretKeys
 {
     [JsonPropertyName("keys")] public List<string> Keys { get; init; } = new();
     [JsonPropertyName("lastChangedDate")] public string LastChangedDate { get; init; } = "";
