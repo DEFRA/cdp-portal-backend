0e8d99778401b8e26710fc24de1bfbeeb576b4ae
Phil Segal <phil@phisec.co.uk>
2024-12-18 15:31:00 +0000

    485559: Add test

--- Defra.Cdp.Backend.Api.Tests/Services/GitHubWorkflowEvents/Model/EventTest.cs
+++ Defra.Cdp.Backend.Api.Tests/Services/GitHubWorkflowEvents/Model/EventTest.cs
@@ -125,4 +125,43 @@ public class EventTest
         Assert.Equal("service.gov.uk", workflowEvent?.Payload?.Services[1].Urls[0].Domain);
         Assert.Equal("service-b", workflowEvent?.Payload?.Services[1].Urls[0].Host);
     }
+
+    [Fact]
+    public void WillDeserializSquidProxyConfigEvent()
+    {
+        var messageBody = """
+                          { "eventType": "squid-proxy-config",
+                            "timestamp": "2024-10-23T15:10:10.123",
+                            "payload": {
+                              "environment": "test",
+                              "default_domains": [".cdp-int.defra.cloud", ".amazonaws.com", "login.microsoftonline.com", "www.gov.uk"],
+                              "services": [
+                                  {"name": "cdp-dotnet-tracing", "allowed_domains": []},
+                                  {"name": "phi-frontend", "allowed_domains": ["gd.eppo.int"]}
+                              ]
+                            }
+                          }
+                          """;
+
+        var workflowEvent = JsonSerializer.Deserialize<Event<SquidProxyConfigPayload>>(messageBody);
+
+        Assert.Equal("squid-proxy-config", workflowEvent?.EventType);
+        Assert.Equal("test", workflowEvent?.Payload?.Environment);
+
+        Assert.Equal(new DateTime(2024, 10, 23, 15, 10, 10, 123), workflowEvent?.Timestamp);
+
+        Assert.Equal(4, workflowEvent?.Payload?.DefaultDomains.Count);
+        Assert.Equal(".cdp-int.defra.cloud", workflowEvent?.Payload?.DefaultDomains[0]);
+        Assert.Equal(".amazonaws.com", workflowEvent?.Payload?.DefaultDomains[1]);
+        Assert.Equal("login.microsoftonline.com", workflowEvent?.Payload?.DefaultDomains[2]);
+        Assert.Equal("www.gov.uk", workflowEvent?.Payload?.DefaultDomains[3]);
+        
+        Assert.Equal(2, workflowEvent?.Payload?.Services.Count);
+        Assert.Equal("cdp-dotnet-tracing", workflowEvent?.Payload?.Services[0].Name);
+        Assert.Equal(0, workflowEvent?.Payload?.Services[0].AllowedDomains.Count);
+        
+        Assert.Equal("phi-frontend", workflowEvent?.Payload?.Services[1].Name);
+        Assert.Equal(1, workflowEvent?.Payload?.Services[1].AllowedDomains.Count);
+        Assert.Equal("gd.eppo.int", workflowEvent?.Payload?.Services[1].AllowedDomains[0]);
+    }
 }
