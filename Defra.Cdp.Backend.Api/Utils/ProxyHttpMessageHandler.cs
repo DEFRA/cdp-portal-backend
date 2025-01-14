8702117c2ee4da9d333d3e09705f336ea0077bde
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-01-14 15:19:44 +0000

    Switches over to forward-proxy

--- Defra.Cdp.Backend.Api/Utils/ProxyHttpMessageHandler.cs
+++ Defra.Cdp.Backend.Api/Utils/ProxyHttpMessageHandler.cs
@@ -6,28 +6,17 @@ public class ProxyHttpMessageHandler : HttpClientHandler
 {
     public ProxyHttpMessageHandler(ILogger<ProxyHttpMessageHandler> logger)
     {
-        var proxyUri = Environment.GetEnvironmentVariable("CDP_HTTPS_PROXY");
+        var proxyUri = Environment.GetEnvironmentVariable("HTTP_PROXY");
         var proxy = new WebProxy { BypassProxyOnLocal = true };
         if (proxyUri != null)
         {
             logger.LogDebug("Creating proxy http client");
-            var uri = new UriBuilder(proxyUri);
-
-            var credentials = GetCredentialsFromUri(uri);
-            if (credentials != null)
-            {
-                logger.LogDebug("Setting proxy credentials");
-                proxy.Credentials = credentials;
-            }
-
-            // Remove credentials from URI to so they don't get logged.
-            uri.UserName = "";
-            uri.Password = "";
-            proxy.Address = uri.Uri;
+            var uri = new UriBuilder(proxyUri).Uri;
+            proxy.Address = uri;
         }
         else
         {
-            logger.LogWarning("CDP_HTTP_PROXY is NOT set, proxy client will be disabled");
+            logger.LogWarning("HTTP_PROXY is NOT set, proxy client will be disabled");
         }
 
         Proxy = proxy;
