a2f76c588f10e2d1aa4dc4628a657427e41618ac
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-02-17 17:28:00 +0000

    moves ServiceTest to utils folder

--- Defra.Cdp.Backend.Api.IntegrationTests/Utils/ServiceTest.cs
+++ Defra.Cdp.Backend.Api.IntegrationTests/Utils/ServiceTest.cs
@@ -2,7 +2,7 @@ using System.Text.Json;
 using Defra.Cdp.Backend.Api.IntegrationTests.Mongo;
 using Defra.Cdp.Backend.Api.Models;
 
-namespace Defra.Cdp.Backend.Api.IntegrationTests.GithubWorkflowEvents;
+namespace Defra.Cdp.Backend.Api.IntegrationTests.Utils;
 
 public abstract class ServiceTest : IClassFixture<MongoIntegrationTest>
 {
