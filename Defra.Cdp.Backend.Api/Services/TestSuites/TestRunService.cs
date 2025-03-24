706a121addfa2d45114838557ed334a4351c5a35
Ben Chidgey <ben@feedmypixel.com>
2025-03-24 18:14:27 +0000

    CORE-317: Add pagination to test runs

--- Defra.Cdp.Backend.Api/Services/TestSuites/TestRunService.cs
+++ Defra.Cdp.Backend.Api/Services/TestSuites/TestRunService.cs
@@ -1,26 +1,34 @@
 using Defra.Cdp.Backend.Api.Models;
 using Defra.Cdp.Backend.Api.Mongo;
 using MongoDB.Driver;
+using InsertOneOptions = MongoDB.Driver.InsertOneOptions;
 
 namespace Defra.Cdp.Backend.Api.Services.TestSuites;
 
 public interface ITestRunService
 {
-    public Task<TestRun?> FindTestRun(string runId, CancellationToken cancellationToken);
-    public Task<List<TestRun>> FindTestRunsForTestSuite(string suite, int limit, CancellationToken cancellationToken);
-    public Task<Dictionary<string, TestRun>> FindLatestTestRuns(CancellationToken cancellationToken);
-    public Task<TestRun?> FindByTaskArn(string taskArn, CancellationToken cancellationToken);
-    public Task CreateTestRun(TestRun testRun, CancellationToken cancellationToken);
-    public Task<TestRun?> Link(TestRunMatchIds ids, string taskArn, CancellationToken cancellationToken);
-    public Task UpdateStatus(string taskArn, string taskStatus, string? testStatus, DateTime ecsEventTimestamp, List<FailureReason> failureReasons, CancellationToken cancellationToken);
-    Task Decommission(string serviceName, CancellationToken cancellationToken);
+    public Task<TestRun?> FindTestRun(string runId, CancellationToken ct);
+
+    public Task<Paginated<TestRun>> FindTestRunsForTestSuite(string suite, int offset = 0, int page = 0, int size = 0,
+        CancellationToken ct = new());
+
+    public Task<Dictionary<string, TestRun>> FindLatestTestRuns(CancellationToken ct);
+    public Task<TestRun?> FindByTaskArn(string taskArn, CancellationToken ct);
+    public Task CreateTestRun(TestRun testRun, CancellationToken ct);
+    public Task<TestRun?> Link(TestRunMatchIds ids, string taskArn, CancellationToken ct);
+
+    public Task UpdateStatus(string taskArn, string taskStatus, string? testStatus, DateTime ecsEventTimestamp,
+        List<FailureReason> failureReasons, CancellationToken ct);
+
+    Task Decommission(string serviceName, CancellationToken ct);
 }
 
 public class TestRunService : MongoService<TestRun>, ITestRunService
 {
     private const string CollectionName = "testruns";
-
     private readonly TimeSpan _ecsLinkTimeWindow = TimeSpan.FromSeconds(120); // how many seconds between requesting the test run and the first ECS event
+    public static readonly int DefaultPageSize = 50;
+    public static readonly int DefaultPage = 1;
     
     public TestRunService(IMongoDbClientFactory connectionFactory, ILoggerFactory loggerFactory) : base(connectionFactory, CollectionName, loggerFactory) {}
 
@@ -33,38 +41,61 @@ public class TestRunService : MongoService<TestRun>, ITestRunService
         return [runIdIndex, suiteIndex, arnIndex];
     }
 
-    public async Task<TestRun?> FindTestRun(string runId, CancellationToken cancellationToken)
+    public async Task<TestRun?> FindTestRun(string runId, CancellationToken ct)
     {
-        return await Collection.Find(t => t.RunId == runId).FirstOrDefaultAsync(cancellationToken);
+        return await Collection.Find(t => t.RunId == runId).FirstOrDefaultAsync(ct);
     }
 
-    public async Task<List<TestRun>> FindTestRunsForTestSuite(string suite, int limit, CancellationToken cancellationToken)
+    public async Task<Paginated<TestRun>> FindTestRunsForTestSuite(
+        string suite,
+        int offset,
+        int page,
+        int size,
+        CancellationToken ct)
     {
-        return await Collection.Find(t => t.TestSuite == suite).SortByDescending(t=>t.Created).Limit(limit).ToListAsync(cancellationToken);
+        var builder = Builders<TestRun>.Filter;
+        var filter = builder.Empty;
+
+        if (!string.IsNullOrWhiteSpace(suite))
+        {
+            var testRunFilter = builder.Where(t => t.TestSuite == suite);
+            filter &= testRunFilter;
+        }
+
+        var testRuns = await Collection
+            .Find(t => t.TestSuite == suite)
+            .Skip(offset + size * (page - DefaultPage))
+            .Limit(size)
+            .SortByDescending(t => t.Created)
+            .ToListAsync(ct);
+
+        var totalTestRuns = await Collection.CountDocumentsAsync(filter, cancellationToken: ct);
+        var totalPages = Math.Max(1, (int)Math.Ceiling((double)totalTestRuns / size));
+
+        return new Paginated<TestRun>(testRuns, page, size, totalPages);
     }
 
-   public async Task<Dictionary<string, TestRun>> FindLatestTestRuns(CancellationToken cancellationToken)
+    public async Task<Dictionary<string, TestRun>> FindLatestTestRuns(CancellationToken ct)
    {
-           var pipeline = new EmptyPipelineDefinition<TestRun>()
-               .Sort(new SortDefinitionBuilder<TestRun>().Descending(d => d.Created))
-               .Group(t => t.TestSuite, grp => new { Root = grp.First() })
-               .Project(grp => grp.Root);
+       var pipeline = new EmptyPipelineDefinition<TestRun>()
+           .Sort(new SortDefinitionBuilder<TestRun>().Descending(d => d.Created)).Group(t => t.TestSuite,
+               grp => new { Root = grp.First() }).Project(grp => grp.Root);
 
-           var runs = await Collection.AggregateAsync(pipeline, cancellationToken: cancellationToken);
-           return runs.ToEnumerable(cancellationToken).ToDictionary(d => d.TestSuite, d => d);
+       var runs = await Collection.AggregateAsync(pipeline, cancellationToken: ct);
+       return runs.ToEnumerable(ct).ToDictionary(d => d.TestSuite, d => d);
    }
 
-    public async Task<TestRun?> FindByTaskArn(string taskArn, CancellationToken cancellationToken)
+    public async Task<TestRun?> FindByTaskArn(string taskArn, CancellationToken ct)
     {
-        return await Collection.Find(t => t.TaskArn == taskArn).FirstOrDefaultAsync(cancellationToken);
+        return await Collection.Find(t => t.TaskArn == taskArn).FirstOrDefaultAsync(ct);
     }
 
-    public async Task CreateTestRun(TestRun testRun, CancellationToken cancellationToken)
+    public async Task CreateTestRun(TestRun testRun, CancellationToken ct)
     {
-        await Collection.InsertOneAsync(testRun, new InsertOneOptions(), cancellationToken);
+        await Collection.InsertOneAsync(testRun, new InsertOneOptions(), ct);
    }
 
-    public async Task<TestRun?> Link(TestRunMatchIds ids, string taskArn, CancellationToken cancellationToken)
+    public async Task<TestRun?> Link(TestRunMatchIds ids, string taskArn, CancellationToken ct)
     {
         var fb = new FilterDefinitionBuilder<TestRun>();
 
@@ -80,26 +111,30 @@ public class TestRunService : MongoService<TestRun>, ITestRunService
             .Update
             .Set(d => d.TaskArn, taskArn);
 
-        return await Collection.FindOneAndUpdateAsync(filter, update, cancellationToken: cancellationToken);
+        return await Collection.FindOneAndUpdateAsync(  filter, update,
+            cancellationToken: ct);
     }
 
-    public async Task UpdateStatus(string taskArn, string taskStatus, string? testStatus, DateTime ecsEventTimestamp, List<FailureReason> failureReasons, CancellationToken cancellationToken)
+    public async Task UpdateStatus(string taskArn, string taskStatus, string? testStatus, DateTime ecsEventTimestamp,
+        List<FailureReason> failureReasons, CancellationToken ct)
     {
-        var update = Builders<TestRun>.Update
-            .Set(t => t.TaskStatus, taskStatus)
-            .Set(t => t.TaskLastUpdate, ecsEventTimestamp)
-            .Set(t => t.FailureReasons, failureReasons);
+        var update = Builders<TestRun>
+            .Update
+            .Set(t => t.TaskStatus, taskStatus).Set(  t => t.TaskLastUpdate, ecsEventTimestamp).Set(
+                t => t.FailureReasons,
+                failureReasons);
 
         if (testStatus != null)
         {
-            update = update.Set(t => t.TestStatus, testStatus);
+            update = update.Set(  t => t.TestStatus, testStatus);
         }
-        
-        await Collection.UpdateOneAsync(t => t.TaskArn == taskArn, update, cancellationToken: cancellationToken);
+
+        await Collection.UpdateOneAsync(  t => t.TaskArn == taskArn, update,
+            cancellationToken: ct);
     }
 
-    public async Task Decommission(string serviceName, CancellationToken cancellationToken)
+    public async Task Decommission(string serviceName, CancellationToken ct)
     {
-        await Collection.DeleteManyAsync(t => t.TestSuite == serviceName, cancellationToken: cancellationToken);
+        await Collection.DeleteManyAsync(  t => t.TestSuite == serviceName, ct);
     }
 }
