1874a44957b3719ec0906f510e70453d9a486f8a
Ben Chidgey <ben@feedmypixel.com>
2025-04-09 16:53:34 +0100

    CORE-416: Update displayName used as Auto* users

--- Defra.Cdp.Backend.Api/Services/AutoTestRunTriggers/AutoTestRunTriggerEventHandler.cs
+++ Defra.Cdp.Backend.Api/Services/AutoTestRunTriggers/AutoTestRunTriggerEventHandler.cs
@@ -6,6 +6,11 @@ using Defra.Cdp.Backend.Api.Utils.Clients;
 
 namespace Defra.Cdp.Backend.Api.Services.AutoTestRunTriggers;
 
+internal static class AutoTestRunConstants
+{
+    public const string AutoTestRunId = "11111111-1111-1111-1111-111111111111";
+}
+
 public class AutoTestRunTriggerEventHandler(
     IDeploymentsService deploymentsService,
     IAutoTestRunTriggerService autoTestRunTriggerService,
@@ -16,36 +21,42 @@ public class AutoTestRunTriggerEventHandler(
     public async Task Handle(string id, EcsDeploymentStateChangeEvent ecsEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation(
-            "{id} Handling EcsDeploymentStateChange trigger test runs for {deploymentId}, {name} {reason}",
+            "{Id} Handling EcsDeploymentStateChange trigger test runs for {DeploymentId}, {Name} {Reason}",
             id, ecsEvent.Detail.DeploymentId, ecsEvent.Detail.EventName, ecsEvent.Detail.Reason);
 
         var deployment =
             await deploymentsService.FindDeploymentByLambdaId(ecsEvent.Detail.DeploymentId, cancellationToken);
         if (deployment == null)
         {
-            logger.LogWarning("{id} Deployment {deploymentId} not found", id, ecsEvent.Detail.DeploymentId);
+            logger.LogWarning("{Id} Deployment {DeploymentId} not found", id, ecsEvent.Detail.DeploymentId);
         }
         else if (deployment.Status != DeploymentStatus.Running)
         {
-            logger.LogWarning("{id} Deployment {deploymentId} not running", id, ecsEvent.Detail.DeploymentId);
+            logger.LogWarning("{Id} Deployment {DeploymentId} not running", id, ecsEvent.Detail.DeploymentId);
         }
         else
         {
             var trigger = await autoTestRunTriggerService.FindForService(deployment.Service, cancellationToken);
+            var environmentTestSuites = trigger?.TestSuites
+                .Where(kvp => kvp.Value.Contains(deployment.Environment))
+                .Select(kvp => kvp.Key)
+                .ToList() ?? [];
 
-            var testSuitesOrEmptyList =
-                trigger?.EnvironmentTestSuitesMap.GetValueOrDefault(deployment.Environment, []) ?? [];
-
-            foreach (var testSuite in testSuitesOrEmptyList)
+            foreach (var testSuite in environmentTestSuites)
             {
-                logger.LogInformation("{id} Triggering test run for {deploymentId} {testSuite} in {environment}",
+                logger.LogInformation("{Id} Triggering test run for {DeploymentId} {TestSuite} in {Environment}",
                     id,
                     ecsEvent.Detail.DeploymentId, testSuite, deployment.Environment);
 
                 var testRunSettings =
                     await testRunService.FindTestRunSettings(testSuite, deployment.Environment, cancellationToken);
 
-                await selfServiceOpsClient.TriggerTestSuite(testSuite, deployment.Environment, deployment.User,
+                var userDetails = new UserDetails
+                {
+                    Id = AutoTestRunConstants.AutoTestRunId, DisplayName = "Auto test runner"
+                };
+
+                await selfServiceOpsClient.TriggerTestSuite(testSuite, deployment.Environment, userDetails,
                     testRunSettings, cancellationToken);
             }
         }
