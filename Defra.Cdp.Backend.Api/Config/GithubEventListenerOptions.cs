13d51daee18d84cbd530096fee0f2ac679f30af0
Phil Segal <phil@phisec.co.uk>
2025-06-27 15:12:20 +0100

    CORE-935: Remove unused endpoint - cleanup

--- Defra.Cdp.Backend.Api/Config/GithubEventListenerOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Defra.Cdp.Backend.Api.Config;
-
-public class GithubEventListenerOptions
-{
-    public const string Prefix = "GithubEvents";
-    public string QueueUrl { get; set; } = null!;
-    public bool Enabled { get; set; }
-}
