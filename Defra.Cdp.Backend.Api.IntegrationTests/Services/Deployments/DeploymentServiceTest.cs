9517b84c0c0f1c72b9fcb9d5659385d1cbb1bb8c
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-07-18 17:39:37 +0100

    CORE-701 fails deployments stuck in requested
    Before registering a new deployment, we now check if there are any
    requests stuck in pending for that service/env.
    
    Also switches from Date.Now to Date.UtcNow

--- Defra.Cdp.Backend.Api.IntegrationTests/Services/Deployments/DeploymentServiceTest.cs
+++ Defra.Cdp.Backend.Api.IntegrationTests/Services/Deployments/DeploymentServiceTest.cs
@@ -7,7 +7,9 @@ using Defra.Cdp.Backend.Api.Services.Aws.Deployments;
 using Defra.Cdp.Backend.Api.Services.Deployments;
 using Defra.Cdp.Backend.Api.Services.Github;
 using Defra.Cdp.Backend.Api.Services.Github.ScheduledTasks;
+using Docker.DotNet.Models;
 using Microsoft.Extensions.Logging.Abstractions;
+using MongoDB.Driver;
 using NSubstitute;
 using NSubstitute.ReturnsExtensions;
 
@@ -226,5 +228,66 @@ public class DeploymentServiceTest(MongoIntegrationTest fixture) : ServiceTest(f
         // The most recent running service should be shown
         Assert.Single(result);
         Assert.Equal(deployment2.CdpDeploymentId, result[0].CdpDeploymentId);
+    }
+    
+    [Fact]
+    public async Task CleansUpStuckRequestsOnRegister()
+    {
+        var mongoFactory = new MongoDbClientFactory(Fixture.connectionString, "deploymentsV2");
+        var repositoryService = new RepositoryService(mongoFactory, new NullLoggerFactory());
+        var service = new DeploymentsService(mongoFactory, repositoryService, userServiceFetcher, new NullLoggerFactory());
+
+        var stuckDeployment = new Deployment
+        {
+            CdpDeploymentId = "stuck-messge",
+            Cpu = "1024",
+            Memory = "1024",
+            ConfigVersion = "6bdb2e40b71f65edff09b9e41474780b9c6426d83d9bfa53376025f89260fbf8",
+            Environment = "test",
+            InstanceCount = 1,
+            Service = "test-backend",
+            Version = "0.31.0",
+            User = new UserDetails { Id = "9999-9999-9999", DisplayName = "Stuck user" },
+            Created = DateTime.Now.Subtract(TimeSpan.FromDays(30)),
+            Updated = DateTime.Now.Subtract(TimeSpan.FromDays(30)),
+            Status = DeploymentStatus.Requested
+        };
+        await mongoFactory.GetCollection<Deployment>(DeploymentsService.CollectionName)
+            .InsertOneAsync(stuckDeployment, new InsertOneOptions(), CancellationToken.None);
+
+        var totalDeployments = await mongoFactory.GetCollection<Deployment>(DeploymentsService.CollectionName)
+            .Find(d =>
+                d.Service == stuckDeployment.Service &&
+                d.Environment == stuckDeployment.Environment &&
+                d.Status == DeploymentStatus.Requested).CountDocumentsAsync(CancellationToken.None);
+        
+        Assert.Equal(1, totalDeployments);
+        
+        userServiceFetcher.GetUser(Arg.Any<string>(), Arg.Any<CancellationToken>())
+            .ReturnsNull();
+
+        var deployment = Deployment.FromRequest(new RequestedDeployment
+        {
+            Cpu = "1024",
+            Memory = "1024",
+            ConfigVersion = "e5fa44f2b31c1fb553b6021e7360d07d5d91ff5e",
+            Environment = "test",
+            DeploymentId = Guid.NewGuid().ToString(),
+            InstanceCount = 1,
+            Service = "test-backend",
+            Version = "1.0.0",
+            User = new UserDetails { Id = "9999-9999-9999", DisplayName = "Test User", }
+        });
+        await service.RegisterDeployment(deployment, new CancellationToken());
+
+        // check its registered the new deployment
+        var result = await service.FindDeployment(deployment.CdpDeploymentId, new CancellationToken());
+        Assert.NotNull(result);
+        Assert.Equal([], result.Audit?.ServiceOwners);
+        Assert.Null(result.Audit?.User);
+        
+        // Check its removed the old requested deployment
+        var stuck = await service.FindDeployment(stuckDeployment.CdpDeploymentId, new CancellationToken());
+        Assert.Equal(DeploymentStatus.Failed, stuck?.Status);
     }
 }
