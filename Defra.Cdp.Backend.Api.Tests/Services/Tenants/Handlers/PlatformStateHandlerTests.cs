affa153f6f7f8325d20701ae2d0d367376b03c0c
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-10-15 14:02:48 +0100

    CORE-1450 cleanup, tests etc

---
+++ Defra.Cdp.Backend.Api.Tests/Services/Tenants/Handlers/PlatformStateHandlerTests.cs
@@ -0,0 +1,80 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Defra.Cdp.Backend.Api.Services.Tenants;
+using Defra.Cdp.Backend.Api.Services.Tenants.Handlers;
+using Defra.Cdp.Backend.Api.Services.Tenants.Models;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+
+namespace Defra.Cdp.Backend.Api.Tests.Services.Tenants.Handlers;
+
+
+public record TestPayload
+{
+    public string Name { get; set; }
+}
+
+public class PlatformStateHandlerTests
+{
+    private ITenantService tenantService = Substitute.For<ITenantService>();
+
+    private string minimalPayload = 
+        """
+        { 
+            "payload_version": "11111111111111111",
+            "payload": {
+                "created": "2025-10-14T15:20:03.747Z",
+                "version": 1,
+                "environment": "dev",
+                "terraform_serials": {
+                  "tfsvcinfra": 1,
+                  "tfvanityurl": 2,
+                  "tfwaf": 3,
+                  "tfopensearch": 4,
+                  "tfgrafana": 5
+                },
+                "tenants": {}
+            },
+            "compression": null
+        }
+        """;
+    
+    private string minimalPayloadCompressed = 
+        """
+        { 
+            "payload_version": "11111111111111111",
+            "payload": "H4sIAAAAAAAAAz2OwQ6CMBBE73wF4SymLRATvsOTF7OBrTaBrdnWGkP4d7eoHOfN7MwuRVlWAyNEHKu+rIwyXa1Vrduz7nqjetUcT+3pUh1yMCEH50mCetNIybGnGSnm4xHTNxeRGazn+RqQHUxB3EWMbNmQBkeW4d+ywQTk4vvJk1Cz0xdY0c2u/QMpIPBwF9zu+CZjQLmwE7T+XiCguA2vxVp8ABqBllDoAAAA",
+            "compression": "gzip",
+            "encoding": "base64"
+        }
+        """;
+
+    
+    [Fact]
+    public async Task TestDecompressAndDeserialize()
+    {
+        // Test data generated via: echo -n '{ "Name": "test-value" }' | gzip | base64 -w 0
+        var result = await PlatformStateHandler.DecompressAndDeserialize<TestPayload>("H4sIAAAAAAAAA6tWUPJLzE1VslJQKkktLtEtS8wpTVVSqOUCAKDGhjEZAAAA");
+        Assert.Equal("test-value", result.Name);
+    }
+
+    [Fact]
+    public async Task TestUncompressedHandleMessage()
+    {
+        var handler = new PlatformStateHandler(tenantService, new NullLoggerFactory());
+        var payload = JsonSerializer.Deserialize<JsonElement>(minimalPayload);
+        await handler.HandleAsync(payload, CancellationToken.None);
+        await tenantService.Received().UpdateState(Arg.Any<PlatformStatePayload>(), Arg.Any<CancellationToken>());
+    }
+    
+    
+    [Fact]
+    public async Task TestCompressedPayload()
+    {
+        var handler = new PlatformStateHandler(tenantService, new NullLoggerFactory());
+        var payload = JsonSerializer.Deserialize<JsonElement>(minimalPayloadCompressed);
+        await handler.HandleAsync(payload, CancellationToken.None);
+        await tenantService.Received().UpdateState(Arg.Any<PlatformStatePayload>(), Arg.Any<CancellationToken>());
+    }
+}
--- Defra.Cdp.Backend.Api/Services/Tenants/Handlers/PlatformStateHandler.cs
+++ Defra.Cdp.Backend.Api/Services/Tenants/Handlers/PlatformStateHandler.cs
@@ -1,38 +1,71 @@
 using System.IO.Compression;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Defra.Cdp.Backend.Api.Services.MonoLambdaEvents;
+using Defra.Cdp.Backend.Api.Services.Tenants.Models;
+using ThirdParty.Json.LitJson;
+using JsonException = System.Text.Json.JsonException;
 
 namespace Defra.Cdp.Backend.Api.Services.Tenants.Handlers;
 
-public class PlatformStateHandler(TenantService tenantService) : IMonoLambdaEventHandler
+class Header
+{
+    [property: JsonPropertyName("payload_version")]
+    public string? PayloadVersion { get; init; }
+    [property: JsonPropertyName("compression")]
+    public string? Compression { get; init; }
+}
+
+public class PlatformStateHandler(ITenantService tenantService, ILoggerFactory loggerFactory) : IMonoLambdaEventHandler
 {
     public string EventType => "platform_state";
+
+    private readonly ILogger<PlatformStateHandler> _logger = loggerFactory.CreateLogger<PlatformStateHandler>();
     
     public async Task HandleAsync(JsonElement message, CancellationToken cancellationToken)
     {
-
+        var header = message.Deserialize<Header>();
+        if (header == null)
+        {
+            throw new Exception("Failed to parse platform state headers");
+        }
+        
+        // Compare received version vs supported version.
+        if (header.PayloadVersion != TenantDataVersion.Version)
+        {
+            // Mismatch doesn't always mean it won't work, but its worth warning.
+            _logger.LogWarning("Platform State payload version mismatch got: {ReceivedVersion} wanted {CurrentVersion}. Consider regenerating the C# classes.", header.PayloadVersion, TenantDataVersion.Version);
+        }
+        
         if (!message.TryGetProperty("payload", out var payload))
         {
             throw new Exception("Platform state payload is missing a payload or was not a string");
         }
 
         // Check if payload is compressed
-        message.TryGetProperty("compression", out var compression);
-        var state = compression.GetString() switch
+        var state = header.Compression switch
         {
-            null => payload.Deserialize<PlatformStatePayload>(),
-            "gzip" => DecompressAndDeserialize<PlatformStatePayload>(payload.GetString() ?? ""),
-            _ => throw new Exception($"Unsupported compression {compression}")
+            null   => payload.Deserialize<PlatformStatePayload>(),
+            "gzip" => await DecompressAndDeserialize<PlatformStatePayload>(payload.GetString() ?? ""),
+            _      => throw new Exception($"Unsupported compression {header.Compression}")
         };
 
-        if (state != null)
+        if (state == null)
         {
-            // TODO: check version/serials to decide if we can/should update
-            await tenantService.UpdateState(state, cancellationToken);
+            throw new Exception("Platform State payload was null!");
         }
+        
+        _logger.LogInformation("Payload model version {Version}", header.PayloadVersion);
+        _logger.LogInformation("tf-svc-infra serial {Serial}", state.TerraformSerials.Tfsvcinfra);
+        _logger.LogInformation("Grafana serial {Serial}", state.TerraformSerials.Tfgrafana);
+        _logger.LogInformation("Opensearch serial {Serial}", state.TerraformSerials.Tfopensearch);
+        _logger.LogInformation("VanityUrls serial {Serial}", state.TerraformSerials.Tfvanityurl);
+        _logger.LogInformation("WAF serial {Serial}", state.TerraformSerials.Tfwaf);
+        
+        await tenantService.UpdateState(state, cancellationToken);
     }
 
-    static T DecompressAndDeserialize<T>(string base64CompressedData) where T : new()
+    public static async Task<T> DecompressAndDeserialize<T>(string base64CompressedData) where T : new()
     {
         if (string.IsNullOrEmpty(base64CompressedData))
         {
@@ -42,9 +75,9 @@ public class PlatformStateHandler(TenantService tenantService) : IMonoLambdaEven
         var compressedBytes = Convert.FromBase64String(base64CompressedData);
         
         using var compressedStream = new MemoryStream(compressedBytes);
-        using var decompressedStream = new GZipStream(compressedStream, CompressionMode.Decompress);
+        await using var decompressedStream = new GZipStream(compressedStream, CompressionMode.Decompress);
 
-        var result = JsonSerializer.Deserialize<T>(decompressedStream);
+        var result = await JsonSerializer.DeserializeAsync<T>(decompressedStream);
         if (result == null)
         {
             throw new JsonException("Deserialization resulted in a null object, check the JSON structure.");
