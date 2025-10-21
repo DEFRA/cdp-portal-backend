ec64a620511e6042d89a3b1fa2636c0a4657b443
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-10-21 13:13:01 +0100

    Adds integration tests

---
+++ Defra.Cdp.Backend.Api.IntegrationTests/Services/EventHistory/EventHistoryRepositoryTest.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+using Defra.Cdp.Backend.Api.IntegrationTests.Mongo;
+using Defra.Cdp.Backend.Api.IntegrationTests.Utils;
+using Defra.Cdp.Backend.Api.Mongo;
+using Defra.Cdp.Backend.Api.Services.EventHistory;
+using Microsoft.Extensions.Logging.Abstractions;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Defra.Cdp.Backend.Api.IntegrationTests.Services.EventHistory;
+
+public class EventHistoryRepositoryTest(MongoIntegrationTest fixture) : ServiceTest(fixture)
+{
+    [Fact]
+    public async Task Persists_json_events()
+    {
+        var mongoFactory = new MongoDbClientFactory(Fixture.connectionString, GetType().Name);
+        var factory = new EventHistoryFactory(mongoFactory, new NullLoggerFactory());
+        var service = factory.Create<EventHistoryRepositoryTest>();
+
+        
+        var msg = JsonDocument.Parse("{\"foo\": 123, \"bar\": \"baz\"}");
+        await service.PersistEvent("1234", msg.RootElement, CancellationToken.None);
+        
+        var colName = nameof(EventHistoryRepositoryTest).ToLower() + "_events";
+
+        var col = mongoFactory.GetCollection<BsonDocument>(colName);
+        var result = col.Find(FilterDefinition<BsonDocument>.Empty).FirstOrDefault(CancellationToken.None);
+
+        Assert.NotNull(result);
+        Assert.Equal("1234", result.GetValue("messageId").AsString);
+        var savedEvent = result.GetValue("event").AsBsonDocument;
+        Assert.Equal(123, savedEvent.GetValue("foo").AsInt32);
+        Assert.Equal("baz", savedEvent.GetValue("bar").AsString);
+    }
+}
