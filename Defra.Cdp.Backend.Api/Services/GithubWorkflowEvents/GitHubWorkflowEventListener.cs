c65c81770df2af08b6e4b7811bd283bfa91a78b6
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-12-10 13:14:47 +0000

    Refactors github event listeners

--- Defra.Cdp.Backend.Api/Services/GithubWorkflowEvents/GitHubWorkflowEventListener.cs
+++ Defra.Cdp.Backend.Api/Services/GithubWorkflowEvents/GitHubWorkflowEventListener.cs
@@ -9,20 +9,51 @@ using Microsoft.Extensions.Options;
 
 namespace Defra.Cdp.Backend.Api.Services.GithubWorkflowEvents;
 
+
+public interface IGithubWorkflowEventHandler
+{
+    public string EventType { get; }
+    public Task Handle(string message, CancellationToken cancellation);
+}
+
 /**
- * Listens for events sent by Github Workflows
+ * Listens for events sent by GitHub Workflows
  * Messages are sent by the workflows and contain event specific payloads
  */
-public class GithubWorkflowEventListener(
-    IAmazonSQS sqs,
-    IOptions<GithubWorkflowEventListenerOptions> config,
-    IGithubWorkflowEventHandler eventHandler,
-    ILogger<GithubWorkflowEventListener> logger)
-    : SqsListener(sqs, config.Value.QueueUrl, logger)
-{
+public class GithubWorkflowEventListener : SqsListener {
+    
+    private readonly Dictionary<string, IGithubWorkflowEventHandler> _handlers = new();
+    private readonly ILogger<GithubWorkflowEventListener> _logger;
+    
+    public GithubWorkflowEventListener(
+        IAmazonSQS sqs,
+        IOptions<GithubWorkflowEventListenerOptions> config,
+        IEnumerable<IGithubWorkflowEventHandler> eventHandlers,
+        ILoggerFactory loggerFactory) : base(sqs, config.Value.QueueUrl, loggerFactory.CreateLogger<GithubWorkflowEventListener>())
+    {
+        _logger = loggerFactory.CreateLogger<GithubWorkflowEventListener>();
+        
+        // Injecting IEnumerable<IGithubWorkflowEventHandler> will provide all registered handlers.
+        _logger.LogInformation("RegisteringIGithubWorkflowEventHandlers");
+        foreach (var h in eventHandlers)
+        {
+            if (!_handlers.TryAdd(h.EventType, h))
+            {
+                _logger.LogError("Failed to register {NewHandler} for event {Event}, already registered to {Handler}",
+                    h.GetType().FullName,
+                    h.EventType,
+                    _handlers[h.EventType].GetType().FullName
+                );
+                throw new ArgumentException($"Duplicate IGithubWorkflowEventHandlers registered for event type {h.EventType}");
+            }
+            _logger.LogInformation("Registered IGithubWorkflowEventHandlers handler for {EventType} to {HandlerName}", h.EventType, h.GetType().Name);
+        }
+        _logger.LogInformation("Registered {Count} IGithubWorkflowEventHandlers", _handlers.Count);
+    }
+    
     protected override async Task HandleMessageAsync(Message message, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Received message from {QueueUrl}: {Id}", QueueUrl, message.MessageId);
+        _logger.LogInformation("Received message from {QueueUrl}: {Id}", QueueUrl, message.MessageId);
 
         try
         {
@@ -30,7 +61,7 @@ public class GithubWorkflowEventListener(
         }
         catch (Exception e)
         {
-            logger.LogError("Failed to process message {Id} {Exception}", message.MessageId, e.Message);
+            _logger.LogError("Failed to process message {Id} {Exception}", message.MessageId, e.Message);
         }
     }
 
@@ -38,18 +69,26 @@ public class GithubWorkflowEventListener(
     {
         var sw = Stopwatch.StartNew();
         var eventWrapper = TryParseMessageBody(message.Body);
-        logger.LogInformation("Message from {QueueUrl}: {Id} took {ElapsedMilliseconds}ms to parse",
+        _logger.LogInformation("Message from {QueueUrl}: {Id} took {ElapsedMilliseconds}ms to parse",
             QueueUrl, message.MessageId, sw.ElapsedMilliseconds);
-        if (eventWrapper != null)
+        if (eventWrapper == null)
         {
-            await eventHandler.Handle(eventWrapper, message.Body, cancellationToken);
-            sw.Stop();
-            logger.LogInformation("Message from {QueueUrl}: {Id} took {ElapsedMilliseconds}ms to handle",
-                QueueUrl, message.MessageId, sw.ElapsedMilliseconds);
+            _logger.LogInformation("Message from {QueueUrl}: {Id} was not readable: {Body}", QueueUrl,
+                message.MessageId, message.Body);
+            return;
+        }
+
+        if (_handlers.TryGetValue(eventWrapper.EventType, out var handler))
+        {
+            await handler.Handle(message.Body, cancellationToken);
         }
         else
-            logger.LogInformation("Message from {QueueUrl}: {Id} was not readable: {Body}", QueueUrl,
-                message.MessageId, message.Body);
+        {
+            _logger.LogWarning("No handler found for event {EventType}", eventWrapper.EventType);
+        }
+        sw.Stop();
+        _logger.LogInformation("Message from {QueueUrl}: {Id} took {ElapsedMilliseconds}ms to handle",
+            QueueUrl, message.MessageId, sw.ElapsedMilliseconds);
     }
 
     private static CommonEventWrapper? TryParseMessageBody(string body)
