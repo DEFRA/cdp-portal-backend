48c62c70f4c9d4f9d6f6df7e81bb82aa4160b28a
David Beale <david.beale@bealearts.co.uk>
2026-08-03 14:17:06 +0100

    Optional ResourceRequestId for topology resources

--- Defra.Cdp.Backend.Api/Services/Entities/Model/EntityTopology.cs
+++ Defra.Cdp.Backend.Api/Services/Entities/Model/EntityTopology.cs
@@ -3,9 +3,17 @@ using System.Text.Json.Serialization;
 namespace Defra.Cdp.Backend.Api.Services.Entities.Model;
 
 
-public record TopologyResourceLink(string? Service, string? Resource, string? Type, string Access);
+public record TopologyResourceLink(string? Service, string? Resource, string? Type, string? Access)
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? ResourceRequestId { get; set; } = null;
+};
 
-public record TopologyResource(string Name, string Type, string Icon, List<TopologyResourceLink>? Links);
+public record TopologyResource(string Name, string Type, string Icon, List<TopologyResourceLink>? Links)
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? ResourceRequestId { get; set; } = null;
+};
 
 public record TopologyService(string Name,
     [property: JsonConverter(typeof(JsonStringEnumConverter<SubType>))]
