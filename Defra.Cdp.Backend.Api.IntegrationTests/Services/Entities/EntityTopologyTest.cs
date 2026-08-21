9b48e4729fd62fc276f280aa967520c9b0b5a3e8
Lukasz Tymoszczuk <publicperson@gmail.com>
2026-08-21 15:39:11 +0100

    CORE-3787: Use topic subscribers data to link SNS topics to their SQS consumers (#507)
    Replace BuildQueueLookup (scanned all entities' queues, joined by topic name) with reading the new subscribers field directly off each SNS topic.

--- Defra.Cdp.Backend.Api.IntegrationTests/Services/Entities/EntityTopologyTest.cs
+++ Defra.Cdp.Backend.Api.IntegrationTests/Services/Entities/EntityTopologyTest.cs
@@ -117,4 +117,72 @@ public class EntityTopologyTest
         Assert.Equivalent(new TopologyService("foo", SubType.Backend, [], [new TopologyResource("foo-topic", SNS.Name, SNS.Icon, [])]), links[0]);
         Assert.Equivalent(new TopologyService("bar", SubType.Backend, [], [new TopologyResource("bar-queue", SQS.Name, SQS.Icon, [ new TopologyResourceLink("foo", "foo-topic", SNS.Name, "subscription")])]), links[1]);
     }
+
+    [Fact]
+    public void Test_queue_subscriptions_are_built_from_sns_topic_subscribers()
+    {
+        var topics = new List<EntityResource<TenantSnsTopic>>
+        {
+            new(SNS.Name, SNS.Icon, "foo-topic", new TenantSnsTopic
+            {
+                Name = "foo-topic",
+                Subscribers =
+                [
+                    new TenantSnsSubscriber { QueueName = "bar-queue", QueueOwner = "bar" }
+                ]
+            })
+        };
+
+        var owners = new Dictionary<string, (SubType SubType, List<Team> Teams)>
+        {
+            ["bar"] = (SubType.Backend, [])
+        };
+
+        var output = EntityTopologyService.QueueSubscriptionsFromTopicSubscribers(topics, owners);
+
+        Assert.Single(output);
+        Assert.Equal("bar", output[0].Service);
+        Assert.Equal("bar-queue", output[0].Queue);
+        Assert.Equal("foo-topic", output[0].Topic);
+        Assert.Equal(SubType.Backend, output[0].SubType);
+    }
+    
+    [Fact]
+    public void Test_queue_subscriptions_fan_out_across_multiple_topics_and_subscribers()
+    {
+        var topics = new List<EntityResource<TenantSnsTopic>>
+        {
+            new(SNS.Name, SNS.Icon, "topic-one", new TenantSnsTopic
+            {
+                Name = "topic-one",
+                Subscribers =
+                [
+                    new TenantSnsSubscriber { QueueName = "queue-a", QueueOwner = "service-a" },
+                    new TenantSnsSubscriber { QueueName = "queue-b", QueueOwner = "service-b" }
+                ]
+            }),
+            new(SNS.Name, SNS.Icon, "topic-two", new TenantSnsTopic
+            {
+                Name = "topic-two",
+                Subscribers =
+                [
+                    new TenantSnsSubscriber { QueueName = "queue-c", QueueOwner = "service-a" }
+                ]
+            })
+        };
+
+        var owners = new Dictionary<string, (SubType SubType, List<Team> Teams)>
+        {
+            ["service-a"] = (SubType.Backend, [new Team { TeamId = "team-a", Name = "Team A" }]),
+            ["service-b"] = (SubType.Frontend, [new Team { TeamId = "team-b", Name = "Team B" }])
+        };
+
+        var output = EntityTopologyService.QueueSubscriptionsFromTopicSubscribers(topics, owners);
+
+        Assert.Equal(3, output.Count);
+
+        Assert.Contains(output, s => s.Topic == "topic-one" && s.Service == "service-a" && s.Queue == "queue-a" && s.SubType == SubType.Backend);
+        Assert.Contains(output, s => s.Topic == "topic-one" && s.Service == "service-b" && s.Queue == "queue-b" && s.SubType == SubType.Frontend);
+        Assert.Contains(output, s => s.Topic == "topic-two" && s.Service == "service-a" && s.Queue == "queue-c" && s.SubType == SubType.Backend);
+    }
 }
