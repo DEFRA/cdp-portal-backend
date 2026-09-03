ca21e570cdc2e9a7c77505cc325cce8b65041e64
David Beale <david.beale@bealearts.co.uk>
2026-09-03 10:58:38 +0100

    extract name and path

--- Defra.Cdp.Backend.Api/Services/BucketManagement/BucketManagementService.cs
+++ Defra.Cdp.Backend.Api/Services/BucketManagement/BucketManagementService.cs
@@ -40,13 +40,15 @@ public class BucketManagementService(IAmazonS3 s3) : IBucketManagementService
             foreach (var s3Object in response.S3Objects)
             {
                 var isFolder = s3Object.Key.EndsWith('/');
+                var relPath = basePath == "" ? s3Object.Key : s3Object.Key.Replace(basePath, "");
+                var name = isFolder ? s3Object.Key.Split("/")[^2] : s3Object.Key.Split("/")[^1];
 
                 objects.Add(new BucketResource
                 {
-                    Name = s3Object.Key,
+                    Name = name,
                     LastModified = s3Object.LastModified ?? DateTime.Now,
                     Size = s3Object.Size ?? 0,
-                    // Path = s3Object.Key
+                    Path = relPath,
                     IsFolder = isFolder
                 });
             }
