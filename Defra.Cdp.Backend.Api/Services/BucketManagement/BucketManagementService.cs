77832221ffbf1036784087fecf9829b750f8a80b
David Beale <david.beale@bealearts.co.uk>
2026-09-03 12:12:35 +0100

    group folders

--- Defra.Cdp.Backend.Api/Services/BucketManagement/BucketManagementService.cs
+++ Defra.Cdp.Backend.Api/Services/BucketManagement/BucketManagementService.cs
@@ -25,7 +25,7 @@ public class BucketManagementService(IAmazonS3 s3) : IBucketManagementService
             Prefix = fullPath
         };
 
-        List<BucketResource> objects = [];
+        var resources = new SortedDictionary<string, BucketResource>();
         ListObjectsV2Response response;
 
         do
@@ -40,24 +40,45 @@ public class BucketManagementService(IAmazonS3 s3) : IBucketManagementService
             foreach (var s3Object in response.S3Objects)
             {
                 var relPath = basePath == "" ? s3Object.Key : s3Object.Key.Replace(basePath, "");
-                var isFolder = relPath.Contains("/");
-                var name = relPath.Split("/")[0];
+                var currentPath = path == "" ? relPath : relPath.Replace(path, "");
+                var isFolder = currentPath.Contains("/");
+                var name = currentPath.Split("/")[0];
 
-                objects.Add(new BucketResource
-                {
-                    Name = name,
-                    LastModified = s3Object.LastModified ?? DateTime.Now,
-                    Size = s3Object.Size ?? 0,
-                    Path = relPath,
-                    IsFolder = isFolder
-                });
+                if (isFolder) {
+                    if (resources.TryGetValue(name, out var resource))
+                    {
+                        resource.Size += s3Object.Size ?? 0;
+                    }
+                    else
+                    {
+                        resources.Add(name, new BucketResource
+                        {
+                            Name = name,
+                            LastModified = s3Object.LastModified ?? DateTime.Now,
+                            Size = s3Object.Size ?? 0,
+                            Path = $"{path}{name}/",
+                            IsFolder = true
+                        });
+                    }
+
+                } else {
+                    resources.Add(name, new BucketResource
+                    {
+                        Name = name,
+                        LastModified = s3Object.LastModified ?? DateTime.Now,
+                        Size = s3Object.Size ?? 0,
+                        Path = relPath,
+                        IsFolder = false
+                    });
+                }
+            
             }
 
-            request.ContinuationToken = response.NextContinuationToken; // TODO: handle using pagination
+            request.ContinuationToken = response.NextContinuationToken;
         }
         while (response.IsTruncated ?? false);
 
-        return objects;
+        return [.. resources.Values];
     }
 
     public async Task<BucketResourceUrl?> GetBucketResourceUrl(string bucket, string basePath, string path, CancellationToken cancellationToken)
