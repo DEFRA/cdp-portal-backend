a44ad8cda6ef125b73da4038a9093ac356d92bb3
David Beale <david.beale@bealearts.co.uk>
2026-09-04 17:33:33 +0100

    working tests - with filtered fixture

--- Defra.Cdp.Backend.Api.Tests/Services/BucketManagement/BucketManagementServiceTests.cs
+++ Defra.Cdp.Backend.Api.Tests/Services/BucketManagement/BucketManagementServiceTests.cs
@@ -11,32 +11,35 @@ public class BucketManagementServiceTests
 {
     private static readonly string s_bucketName = "test-bucket";
     private static readonly DateTime s_modifiedDate = DateTime.Now;
-    private static readonly ListObjectsV2Response s_s3ListObjectsResponse = new()
-    {
-        IsTruncated = false,
-        S3Objects = [
-            new S3Object {
-                Key = "file.txt",
-                LastModified = s_modifiedDate,
-                Size = 1254
-            },
-            new S3Object {
-                Key = "folder/file-in-folder.txt",
-                LastModified = s_modifiedDate,
-                Size = 751254
-            },
-            new S3Object {
-                Key = "folder/sub-folder/file-in-folder.txt",
-                LastModified = s_modifiedDate,
-                Size = 3452
-            },
-            new S3Object {
-                Key = "folder/empty-folder/",
-                LastModified = s_modifiedDate,
-                Size = 0
-            }
-        ]
-    };
+
+    private static ListObjectsV2Response filteredListResponse(string prefix = "") {
+        return new ListObjectsV2Response
+        {
+            IsTruncated = false,
+            S3Objects = [.. new List<S3Object>([
+                new S3Object {
+                    Key = "file.txt",
+                    LastModified = s_modifiedDate,
+                    Size = 1254
+                },
+                new S3Object {
+                    Key = "folder/file-in-folder.txt",
+                    LastModified = s_modifiedDate,
+                    Size = 751254
+                },
+                new S3Object {
+                    Key = "folder/sub-folder/file-in-folder.txt",
+                    LastModified = s_modifiedDate,
+                    Size = 3452
+                },
+                new S3Object {
+                    Key = "folder/empty-folder/",
+                    LastModified = s_modifiedDate,
+                    Size = 0
+                }
+            ]).Where(o => prefix == "" || o.Key.StartsWith(prefix))]
+        };
+    }
 
     [Fact]
     public async Task Test_list_resources_at_root_path()
@@ -44,7 +47,7 @@ public class BucketManagementServiceTests
         var s3 = Substitute.For<IAmazonS3>();
         var bucketManagementService = Substitute.For<BucketManagementService>(s3);
 
-        s3.ListObjectsV2Async(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(s_s3ListObjectsResponse));
+        s3.ListObjectsV2Async(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(filteredListResponse("")));
 
         var result = await bucketManagementService.ListBucketResources(s_bucketName, "", "", TestContext.Current.CancellationToken);
 
@@ -61,7 +64,7 @@ public class BucketManagementServiceTests
         var s3 = Substitute.For<IAmazonS3>();
         var bucketManagementService = Substitute.For<BucketManagementService>(s3);
 
-        s3.ListObjectsV2Async(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(s_s3ListObjectsResponse));
+        s3.ListObjectsV2Async(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(filteredListResponse("folder/")));
 
         var result = await bucketManagementService.ListBucketResources(s_bucketName, "folder/", "", TestContext.Current.CancellationToken);
 
@@ -79,7 +82,7 @@ public class BucketManagementServiceTests
         var s3 = Substitute.For<IAmazonS3>();
         var bucketManagementService = Substitute.For<BucketManagementService>(s3);
 
-        s3.ListObjectsV2Async(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(s_s3ListObjectsResponse));
+        s3.ListObjectsV2Async(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(filteredListResponse("folder/")));
 
         var result = await bucketManagementService.ListBucketResources(s_bucketName, "", "folder/", TestContext.Current.CancellationToken);
 
@@ -91,19 +94,19 @@ public class BucketManagementServiceTests
         Assert.Equivalent(expected, result, false);
     }
 
-    // [Fact]
-    // public async Task Test_list_resources_with_basePath_and_path()
-    // {
-    //     var s3 = Substitute.For<IAmazonS3>();
-    //     var bucketManagementService = Substitute.For<BucketManagementService>(s3);
+    [Fact]
+    public async Task Test_list_resources_with_basePath_and_path()
+    {
+        var s3 = Substitute.For<IAmazonS3>();
+        var bucketManagementService = Substitute.For<BucketManagementService>(s3);
 
-    //     s3.ListObjectsV2Async(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(s_s3ListObjectsResponse));
+        s3.ListObjectsV2Async(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(filteredListResponse("folder/sub-folder/")));
 
-    //     var result = await bucketManagementService.ListBucketResources(s_bucketName, "folder/", "sub-folder/", TestContext.Current.CancellationToken);
+        var result = await bucketManagementService.ListBucketResources(s_bucketName, "folder/", "sub-folder/", TestContext.Current.CancellationToken);
 
-    //     var expected = new List<BucketResource>([
-    //         new BucketResource { Name = "file-in-folder.txt", Path = "sub-folder/file-in-folder.txt", Size = 3452, ModifiedDate = s_modifiedDate, IsFolder = false },
-    //     ]);
-    //     Assert.Equivalent(expected, result, false);
-    // }
+        var expected = new List<BucketResource>([
+            new BucketResource { Name = "file-in-folder.txt", Path = "sub-folder/file-in-folder.txt", Size = 3452, ModifiedDate = s_modifiedDate, IsFolder = false },
+        ]);
+        Assert.Equivalent(expected, result, false);
+    }
 }
