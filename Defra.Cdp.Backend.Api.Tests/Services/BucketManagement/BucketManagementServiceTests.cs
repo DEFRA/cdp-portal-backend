73f600d2bbe05f32e8e2844ba26c1b36d1a13254
David Beale <david.beale@bealearts.co.uk>
2026-09-09 13:48:55 +0100

    test upload start

--- Defra.Cdp.Backend.Api.Tests/Services/BucketManagement/BucketManagementServiceTests.cs
+++ Defra.Cdp.Backend.Api.Tests/Services/BucketManagement/BucketManagementServiceTests.cs
@@ -10,6 +10,7 @@ public class BucketManagementServiceTests
 {
     private static readonly string s_bucketName = "test-bucket";
     private static readonly DateTime s_modifiedDate = DateTime.Now;
+    private const Int64 ONE_HUNDRED_MEGABYTES = 100 * 1024 * 1024;
 
     private static ListObjectsV2Response filteredListResponse(string prefix = "") {
         return new ListObjectsV2Response
@@ -54,7 +55,7 @@ public class BucketManagementServiceTests
             new BucketResource { Name = "folder", Path = "folder/", Size = 754706, ModifiedDate = s_modifiedDate, IsFolder = true },
             new BucketResource { Name = "file.txt", Path = "file.txt", Size = 1254, ModifiedDate = s_modifiedDate, IsFolder = false },
         ]);
-        Assert.Equivalent(expected, result, false);
+        Assert.Equivalent(expected, result, true);
     }
 
     [Fact]
@@ -72,7 +73,7 @@ public class BucketManagementServiceTests
             new BucketResource { Name = "sub-folder", Path = "sub-folder/", Size = 3452, ModifiedDate = s_modifiedDate, IsFolder = true },
             new BucketResource { Name = "file-in-folder.txt", Path = "file-in-folder.txt", Size = 751254, ModifiedDate = s_modifiedDate, IsFolder = false },
         ]);
-        Assert.Equivalent(expected, result, false);
+        Assert.Equivalent(expected, result, true);
     }
 
     [Fact]
@@ -90,7 +91,7 @@ public class BucketManagementServiceTests
             new BucketResource { Name = "sub-folder", Path = "folder/sub-folder/", Size = 3452, ModifiedDate = s_modifiedDate, IsFolder = true },
             new BucketResource { Name = "file-in-folder.txt", Path = "folder/file-in-folder.txt", Size = 751254, ModifiedDate = s_modifiedDate, IsFolder = false },
         ]);
-        Assert.Equivalent(expected, result, false);
+        Assert.Equivalent(expected, result, true);
     }
 
     [Fact]
@@ -106,7 +107,7 @@ public class BucketManagementServiceTests
         var expected = new List<BucketResource>([
             new BucketResource { Name = "file-in-folder.txt", Path = "sub-folder/file-in-folder.txt", Size = 3452, ModifiedDate = s_modifiedDate, IsFolder = false },
         ]);
-        Assert.Equivalent(expected, result, false);
+        Assert.Equivalent(expected, result, true);
     }
 
     [Fact]
@@ -119,7 +120,7 @@ public class BucketManagementServiceTests
 
         var result = await bucketManagementService.GetBucketResourceUrl(s_bucketName, "folder/", "sub-folder/missing-file.txt", TestContext.Current.CancellationToken);
 
-        Assert.Equivalent(null, result, false);
+        Assert.Equivalent(null, result, true);
     }
 
     [Fact]
@@ -134,7 +135,7 @@ public class BucketManagementServiceTests
         var result = await bucketManagementService.GetBucketResourceUrl(s_bucketName, "folder/", "sub-folder/file-in-folder.txt", TestContext.Current.CancellationToken);
 
         var expected = new BucketResourceUrl { Method = "GET", Url = "https://the-presigned-url.s3.aws.com" };
-        Assert.Equivalent(expected, result, false);
+        Assert.Equivalent(expected, result, true);
     }
 
     [Fact]
@@ -147,6 +148,63 @@ public class BucketManagementServiceTests
 
         var result = await bucketManagementService.GetBucketResourceUrl(s_bucketName, "folder/", "sub-folder/", TestContext.Current.CancellationToken);
 
-        Assert.Equivalent(null, result, false);
+        Assert.Equivalent(null, result, true);
+    }
+
+    [Fact]
+    public async Task Test_start_resource_upload_with_basePath_and_path_using_small_file()
+    {
+        var s3 = Substitute.For<IAmazonS3>();
+        var bucketManagementService = Substitute.For<BucketManagementService>(s3);
+
+        s3.InitiateMultipartUploadAsync(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(new InitiateMultipartUploadResponse { UploadId = "1234" }));
+
+        var result = await bucketManagementService.StartBucketResourceMultipartUpload(s_bucketName, "folder/", "sub-folder/new-file", 45000, TestContext.Current.CancellationToken);
+
+        var expected = new BucketResourceUpload
+        {
+            UploadId = "1234",
+            Parts = [
+                new BucketResourceUploadPart {
+                    PartNumber = 1,
+                    QueryParams = "uploadId=1234&partNumber=1",
+                    ByteStartPosition = 0,
+                    ByteEndPosition = 45000
+                }
+            ]
+        };
+        Assert.Equivalent(expected, result, true);
+        Assert.Equivalent(expected.Parts[0], result.Parts[0], true);
+    }
+
+    [Fact]
+    public async Task Test_start_resource_upload_with_basePath_and_path_using_large_file()
+    {
+        var s3 = Substitute.For<IAmazonS3>();
+        var bucketManagementService = Substitute.For<BucketManagementService>(s3);
+
+        s3.InitiateMultipartUploadAsync(default, TestContext.Current.CancellationToken).ReturnsForAnyArgs(Task.FromResult(new InitiateMultipartUploadResponse { UploadId = "1234" }));
+
+        var result = await bucketManagementService.StartBucketResourceMultipartUpload(s_bucketName, "folder/", "sub-folder/new-file", 145000000, TestContext.Current.CancellationToken);
+
+        var expected = new BucketResourceUpload
+        {
+            UploadId = "1234",
+            Parts = [
+                new BucketResourceUploadPart {
+                    PartNumber = 1,
+                    QueryParams = "uploadId=1234&partNumber=1",
+                    ByteStartPosition = 0,
+                    ByteEndPosition = ONE_HUNDRED_MEGABYTES
+                },
+                new BucketResourceUploadPart {
+                    PartNumber = 2,
+                    QueryParams = "uploadId=1234&partNumber=2",
+                    ByteStartPosition = ONE_HUNDRED_MEGABYTES,
+                    ByteEndPosition = 145000000 - ONE_HUNDRED_MEGABYTES
+                }
+            ]
+        };
+        Assert.Equivalent(expected, result, true);
     }
 }
