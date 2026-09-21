893665dd04695eb0f714c93b5539d41e6a192928
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-09-21 14:51:39 +0100

    Logs unexpected GH statuses on repo lookup

--- Defra.Cdp.Backend.Api/Services/Github/GithubApiService.cs
+++ Defra.Cdp.Backend.Api/Services/Github/GithubApiService.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Defra.Cdp.Backend.Api.Services.Github.ScheduledTasks;
 
 namespace Defra.Cdp.Backend.Api.Services.Github;
@@ -34,6 +35,11 @@ public class GithubApiService(
         var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
         var exists = response.IsSuccessStatusCode;
         logger.LogInformation("Checking if GitHub repo {repo} exists, exists: {exists}", repo, exists);
+        if (response.StatusCode is not (HttpStatusCode.OK or HttpStatusCode.NotFound))
+        {
+            logger.LogWarning("Unexpected response code when checking {repo}: status: {code}", repo, response.StatusCode);
+        }
+        
         await response.Content.CopyToAsync(Stream.Null, cancellationToken);
         return exists;
     }
